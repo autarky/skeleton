aad33c57966208d706d93c6e6d0bf693d191b0c9
Andreas Lutro <anlutro@gmail.com>
2014-04-06 04:32:33 +0200

    set a $base variable for simplicity

--- app/config/path.php
+++ app/config/path.php
@@ -3,21 +3,21 @@ return [
 	/**
 	 * The root path of your project.
 	 */
-	'base' => dirname(dirname(__DIR__)),
+	'base' => $base = dirname(dirname(__DIR__)),
 
 	/**
 	 * The path where your templates are located.
 	 */
-	'templates' => dirname(dirname(__DIR__)).'/templates',
+	'templates' => $base . '/templates',
 
 	/**
 	 * The path where cached template files should be stored.
 	 */
-	'templates-cache' => dirname(dirname(__DIR__)).'/tmp/templates',
+	'templates-cache' => $base . '/tmp/templates',
 
 	/**
 	 * If the 'file' session handler is used, this is the path to the directory
 	 * where session files will be stored.
 	 */
-	'session' => dirname(dirname(__DIR__)).'/tmp/session',
+	'session' => $base . '/tmp/session',
 ];

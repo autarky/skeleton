b31be01f8d4ba7d65bb2f19aba1c061cda8d2152
Andreas Lutro <anlutro@gmail.com>
2014-05-01 14:38:51 +0200

    update to version 0.2

---
+++ app/console.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php5
+<?php
+$app = require __DIR__ .'/start.php';
+
+/**
+ * This creates a console application instance and prepares the application for
+ * a console request.
+ *
+ * You can add custom console commands here (before run() is called) or via your
+ * own custom service providers using the registerConsole method.
+ *
+ * @link http://symfony.com/doc/current/components/console/introduction.html
+ * @link https://github.com/symfony/Console
+ * 
+ * @var \Symfony\Component\Console\Application
+ */
+$console = $app->bootConsole();
+
+$console->run();

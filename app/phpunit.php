7578f06561143333664b68d23a421eea0e2fd84d
Andreas Lutro <anlutro@gmail.com>
2014-06-15 04:31:00 +0200

    tweak default phpunit configs

--- app/phpunit.php
+++ app/phpunit.php
@@ -2,4 +2,5 @@
 // Use Composer's autoloader.
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
+// make sure the default timezone is set to prevent errors.
 date_default_timezone_set('UTC');

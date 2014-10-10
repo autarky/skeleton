ddc3186e183dbaa866bfc913c0ade12175026dd1
Andreas Lutro <anlutro@gmail.com>
2014-10-10 20:51:29 +0200

    add diagnostic check

---
+++ bin/check.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php5
+<?php
+echo <<<INFO
+To get the most out of this script, run it once as your own user and
+once as the web server user: `sudo -u www-data ./bin/check.php`
+\n
+INFO;
+
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+$configPath = dirname(__DIR__).'/app/config';
+
+if (file_exists($configPath.'/path.php')) {
+	$paths = require $configPath.'/path.php';
+} else if (file_exists($configPath.'/path.yml')) {
+	$paths = Symfony\Component\Yaml\Yaml::parse($configPath.'/path.php');
+} else {
+	echo "ERROR: No path config file found!\n";
+	exit(1);
+}
+
+$verbose = in_array('-v', $argv) || in_array('--verbose', $argv);
+
+exit(Autarky\Support\Diagnostic::check($paths, $verbose));
